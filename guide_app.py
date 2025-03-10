cdfc0dba13e07ba3c285bf004db4d3264bafeff8
Janik Schmid <janik.schmid@sap.com>
2025-03-10 11:56:03 +0100

    feat(guide): apply tipps of the linter

--- guide_app.py
+++ guide_app.py
@@ -1,16 +1,22 @@
+'''
+guide file
+'''
+
 import bisect
 
 def result(inputfile):
+    """
+    compute result of exercise
+    """
     list_a = []
     list_b = []
-    with open(inputfile) as f:
+    with open(inputfile, encoding="utf-8") as f:
         for line in f:
             splits = line.strip().split('   ')
             bisect.insort(list_a, int(splits[0]))
             bisect.insort(list_b, int(splits[1]))
-        
-    result = 0
-    for i in range(len(list_a)):
-        result += abs(list_a[i] - list_b[i])
-        
-    return result
+    result_of_function = 0
+    for i, list_a_entry in enumerate(list_a):
+        result_of_function += abs(list_a_entry - list_b[i])
+
+    return result_of_function
